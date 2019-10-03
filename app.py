bfd230b0c5fcfdd9cbd039d2bdf36106040ef370
Andrei Sharafullin <sharafullin@gmail.com>
2019-10-03 10:09:37 +0100

    updated app to be hosted in uwsgi

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-first_app = Flask(__name__)
+app = Flask(__name__)
 
-@first_app.route("/")
+@app.route("/")
 def first_function():
  return "<html><body><h1 style='color:red'>I am hosted on Raspberry Pi !!!</h1></body></html>"
 
 if __name__ == "__main__":
- first_app.run(host='0.0.0.0')
+ app.run(host='0.0.0.0')
