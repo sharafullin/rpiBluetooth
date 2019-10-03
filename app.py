641e7ba3b7e3490a3f713a08948ef7542142514a
Andrei Sharafullin <andrei.sharafullin@tomtom.com>
2019-10-03 14:33:35 +0200

    added reboot

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
+import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-payload = {'name':'test'}
-
 @app.route("/")
 def index():
-    return "<html><body><h1 style='color:red'>I am hosted on Raspberry Pi !!!</h1></body></html>"
+    return "<html><body><h1 style='color:red'>I am hosted on Raspberry Pi v4 !!!</h1></body></html>"
 
-@app.route("/api/v1.0/test")
+@app.route("/api/v1.0/test", methods=['GET'])
 def api_get_test():
-    return jsonify(payload);
+    return jsonify({'name':'test'});
+
+@app.route("/api/v1.0/restart", methods=['GET'])
+def api_get_restart():
+    os.system('reboot')
+    return jsonify({'name':'test'});
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
