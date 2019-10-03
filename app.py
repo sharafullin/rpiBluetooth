ce86dc82a6b4648a007bc705126fb1c081dc3e84
Andrei Sharafullin <sharafullin@gmail.com>
2019-10-03 13:12:46 +0100

    updated py app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def first_function():
- return "<html><body><h1 style='color:red'>I am hosted on Raspberry Pi !!!</h1></body></html>"
+ return "<html><body><h1 style='color:red'>I am hosted on Raspberry Pi v3 !!!</h1></body></html>"
 
 if __name__ == "__main__":
  app.run(host='0.0.0.0')
