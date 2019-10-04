d02024c966d9e41f4036407c66d5e0d44cbeda5a
Andrei Sharafullin <andrei.sharafullin@tomtom.com>
2019-10-04 16:51:33 +0200

    test

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 from flask import Flask, jsonify
-import paho.mqtt.client as mqtt
-import paho.mqtt.publish as publish
-import sqlite3
 
 app = Flask(__name__)
 
-client = mqtt.Client()
-client.connect("localhost", 1883, 60)
 
 @app.route("/")
 def index():
@@ -15,11 +10,6 @@ def index():
 @app.route("/api/v1.0/test", methods=['GET'])
 def api_get_test():
     return jsonify({'name':'test'});
-
-@app.route("/api/v1.0/restart", methods=['GET'])
-def api_get_restart():
-    publish.single("paho/restart", "0", hostname="localhost")
-    return ;
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
